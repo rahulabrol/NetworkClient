af5f13b289ae0438281cea9153a8e76511a3c412
Rahul Abrol <abrol@Rahuls-MacBook-Air.local>
2023-08-17 13:20:26 +0530

    deleted files

--- app/src/main/java/com/rahul/networkclient/DogApplication.kt
+++
@@ -1,9 +0,0 @@
-package com.rahul.networkclient
-
-import dagger.hilt.android.HiltAndroidApp
-
-/**
- * Created by abrol at 09/08/23.
- */
-@HiltAndroidApp
-class DogApplication : android.app.Application()
